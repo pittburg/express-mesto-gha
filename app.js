798b994535677ef764e95d4ad05f8eb195a5b57f
Andrey Aleksandrov <pittburg@yandex.ru>
2022-06-10 11:10:38 +0300

    добавить схемы и модели, подключить базу данных

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.get('/', (_req, res) => {
+  res.send({ message: 'привет' });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
