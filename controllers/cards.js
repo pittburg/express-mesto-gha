042c188d39403f38f6d330057cd920b5504dc41f
Andrey Aleksandrov <pittburg@yandex.ru>
2022-06-22 16:37:43 +0300

    refactor/ контроллер cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,7 +3,7 @@ const { BAD_REQUEST, NOT_FOUND, SOME_ERROR } = require('../errors/errors');
 
 const getCards = (req, res) => {
   Card.find({})
-    .then((cards) => res.status(200).send(cards))
+    .then((cards) => res.status(200).send({ data: cards }))
     .catch(() => res.status(SOME_ERROR).send({ message: 'Ошибка сервера' }));
 };
 
@@ -11,7 +11,7 @@ const createCard = (req, res) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.status(200).send(card))
+    .then((card) => res.status(200).send({ data: card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         return res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
